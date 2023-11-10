cc9733b907b909e14ac42999c355632868176bee
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-11-10 17:47:59 +0300

    add host

--- backend/settings_sample.py
+++ backend/settings_sample.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-0gjl4g+mng69grm!f!w@e5#9heig3*9s76d%q8g@5$5i2a=_+$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["hackathon2023.mr-lemontree.site", "localhost"]
+ALLOWED_HOSTS = ["hackathon2023.mr-lemontree.site", "127.0.0.1", "localhost"]
 
 
 # Application definition
