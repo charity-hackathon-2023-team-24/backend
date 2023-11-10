68a0c35b934ffad9572150f906592ea8f7e927ae
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-11-10 22:01:41 +0300

    add simple view

--- api/views.py
+++ api/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.http import HttpRequest, JsonResponse
+
+
+def dummy_json(request: HttpRequest) -> JsonResponse:  # noqa:ARG001
+    return JsonResponse({"title": "hello from backend", "body": "You successfully received data"})
